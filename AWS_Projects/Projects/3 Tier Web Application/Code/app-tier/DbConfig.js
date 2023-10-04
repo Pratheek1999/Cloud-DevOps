23ccf88d055b1fcbdc8e6d9fe1fbc08208dab3f5
Pratheek Hegde <pratheekhegde2017@gmail.com>
2023-10-04 14:56:05 +0530

    Add files via upload

---
+++ AWS_Projects/Projects/3 Tier Web Application/Code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cmdteboom8qa.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'WebAppDB'
+});
